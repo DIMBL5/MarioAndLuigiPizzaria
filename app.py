af4da55a9fc9a7599b8d2e47118d89d0f3fbb5e6
filezz <markcapatina5@gmail.com>
2024-10-14 18:11:34 +0200

    added basic order details fetching

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask , render_template , redirect,request,url_for
+
+app = Flask(__name__)
+
+@app.route("/order") #order page from where we will get information about pizza +  
+def order () : 
+    return render_template("order.html")
+ 
+@app.route("/process",methods =["POST"]) 
+
+def process_order():
+    pizza_type = request.form.get("pizza")
+    quantity = request.form.get("quantity")
+    #table_nr = request.form.get("table")
+    id_nr = request.args.get('id')
+
+    return render_template('placeholder.html',pizza = pizza_type , quan = quantity,id = id_nr)
+    
+
+if __name__ == '__main__' : 
+    app.run(host="0.0.0.0",port=5000,debug =True)#running flask on local network in order to be possible to acces the flask server from the phone
