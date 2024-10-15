1336806bf012b27875787a0dd69741b0da6e99f1
filezz <markcapatina5@gmail.com>
2024-10-15 11:54:50 +0200

    small changes in aspect

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask , render_template , redirect,request,url_for
 
 app = Flask(__name__)
 
-@app.route("/order") #order page from where we will get information about pizza +  
+@app.route("/order") #order page from where we place an order 
 def order () : 
     return render_template("order.html")
  
 @app.route("/process",methods =["POST"]) 
 
-def process_order():
+def process_order(): #processing the order 
     pizza_type = request.form.get("pizza")
     quantity = request.form.get("quantity")
     #table_nr = request.form.get("table")
