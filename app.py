6ea90c25811a0bc80f07c69d02a2dfbb216ffef7
filezz <markcapatina5@gmail.com>
2024-10-17 13:38:49 +0200

    Added sessions in order to fix the issue of mixing orders

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask,jsonify,render_template,request,redirect,url_for
+from flask import Flask,jsonify,render_template,request,redirect,url_for,session
 import sqlite3
 import time
 
 app = Flask(__name__)
+
+file = open("sessionkey.txt","r")
+SESSIONKEY  = file.read() # reading from a gitignore file in order not to leak the key
+
+app.secret_key=SESSIONKEY
+
 
 DATABASE = 'pizza_orders.db'
 connection = sqlite3.connect("pizza_order.db",check_same_thread=False)
@@ -20,40 +26,46 @@ pizzaPrice= {
     'Hawaiian' : 9
 }
 
-tableNr = None
-total_amount = 0
 
 @app.route("/order") # this is the index page, where customers order 
 def menu():
     table_nr = request.args.get('id')
-    global tableNr
-    tableNr = table_nr
-    return render_template('menu.html',table = tableNr)
+    session['tableNr'] = table_nr
+    session['order'] ={}
+    return render_template('menu.html',table = session['tableNr'])
 
 @app.route("/submit_order",methods = ['POST']) 
 def submit():
     pizza_type = request.form.get('pizza') 
+    if 'order' not in session : #if there is no order in session, create new order
+        session['order'] = {}
 
-    if pizza_type in order :
+    order = session['order'] #populate dictionary from session
+
+    if pizza_type in order:
         order[pizza_type] += 1
     else:
-        order[pizza_type] = 1
+        order[pizza_type] = 1 
+
+    session['order'] = order #updating session with data /send back to session
     return jsonify({'message': f'{pizza_type} has been added to your cart!'})
 
 @app.route("/confirm")
 def confirm():
-    global total_amount
-    for pizza,amount in order.items():
-        total_amount = total_amount + pizzaPrice[pizza] * amount
-    return render_template("confirm.html",order = order,table = tableNr,total = total_amount)
+    order = session.get('order',{})
+    total_amount = sum(quantity * pizzaPrice[pizza] for pizza, quantity in order.items())
+    return render_template("confirm.html",order = order,table = session['tableNr'], total = total_amount)
 
 @app.route('/thankyou')
 def thankyou():
+    order = session.get('order' , {}) #if not found returns an empty dict {}
+    tableNr = session['tableNr']
     time_cur = time.strftime('%H:%M:%S')
+
     for pizzaType,amount in order.items():
         cursor.execute("INSERT INTO pizzaOrders (tablenr,pizza_type,quantity,time) VALUES (?,?,?,?)",(tableNr,pizzaType,amount,time_cur))
     
-    order.clear()
+    session.pop('order',None) # clear the dictionary from the session
     connection.commit()
     return render_template('thankyou.html')
 
